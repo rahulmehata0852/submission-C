970cf57f7fda14b70ed8e14073d4c28f64955fe3
rahul mahato <rahulmahato0852@gmail.com>
2024-03-07 16:51:39 +0530

    new db coonected

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, "dist")))
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://submission-c.onrender.com",
     credentials: true
 }))
 
